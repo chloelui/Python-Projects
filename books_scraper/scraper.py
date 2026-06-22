98351bedc4cc46ea47a72fa2fe0a4b6eee7a7c70
Chloe Lui <chloe.lui3225@gmail.com>
2026-06-22 11:11:35 -0700

    clean repo (remove venv and large files)

--- books_scraper/scraper.py
+++ books_scraper/scraper.py
@@ -3,13 +3,69 @@ import pandas as pd
 import time
 
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from time import sleep
+
+BASE_URL = "https://books.toscrape.com/"
+
+def safe_get(url, retries=3):
+    for i in range(retries):
+        try:
+            return requests.get(url, timeout=10)
+        except requests.exceptions.RequestException:
+            print(f"Retrying {url} ({i+1}/{retries})")
+            sleep(2)
+
+    print(f"FAILED permanently: {url}")
+    return None
+
+def get_book_details(url):
+    response = safe_get(url)
+    if response is None:
+        return {
+            "upc": None,
+            "stock": None,
+            "category": None,
+            "description": None
+        }
+
+    soup = BeautifulSoup(response.text,"html.parser")
+
+    # Store info from table
+    table = soup.find("table")
+
+    product_info = {}
+
+    for row in table.find_all("tr"):
+        header = row.th.text
+        value = row.td.text
+        product_info[header] = value
+
+    # Find book category using breadcrumb
+    breadcrumb = soup.find("ul",class_="breadcrumb")
+    category = (breadcrumb.find_all("li")[2].text.strip())
+
+    # Get product description
+    description = ""
+    desc_header = soup.find("div",id="product_description")
+    if desc_header:
+        description = (desc_header.find_next("p").text.strip())
+
+    return {
+        "upc":product_info.get("UPC"),
+        "stock":product_info.get("Availability"),
+        "category":category,
+        "description":description
+    }
 
 all_books = []
 
 # Scrape all 50 pages
 for page in range (1,51):
-    url = (f"https://books.toscrape.com/catalogue/page-{page}.html")
-    response = requests.get(url)
+    page_url = (f"https://books.toscrape.com/catalogue/page-{page}.html")
+    response = safe_get(page_url)
+    if response is None:
+        continue
 
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -23,15 +79,26 @@ for page in range (1,51):
 
         rating = book.find("p", class_="star-rating")["class"][1]
 
+        # Find each book link
+        href = book.h3.a["href"]        # Relative book url
+        book_url = urljoin(BASE_URL + "catalogue/",href)
+
+        # Get individual book details
+        book_details = get_book_details(book_url)
+
         # Create dict for each book
         all_books.append({
             "title":title,
             "price":price,
-            "rating":rating
+            "rating":rating,
+            "category":book_details["category"],
+            "stock":book_details["stock"],
+            "description":book_details["description"],
+            "upc":book_details["upc"]
         })
 
-    print(f"Finished page {page}")
-    time.sleep(1)       # Wait 1 sec between each request
+        print(f"Scraped {title}")
+        time.sleep(0.2)
 
 # Create CSV file
 df = pd.DataFrame(all_books)
